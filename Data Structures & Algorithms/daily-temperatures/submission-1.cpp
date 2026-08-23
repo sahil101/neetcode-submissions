210d8ed0fd60ab2e89da48407a3130eefe102ff2
Sahil Agarwal <agarwalsahil97@gmail.com>
2026-08-23 18:16:17 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+       // monotonic stack
+
+       int n = temperatures.size();
+       stack<int> st;
+       vector<int> result(n, 0);
+
+       for(int i = n - 1; i >= 0; i--) {
+        while (!st.empty() && temperatures[st.top()] <= temperatures[i]) {
+            st.pop();
+        }
+        if (!st.empty()) {
+            result[i] = st.top() - i;
+        } 
+        st.push(i); 
+       } 
+
+       return result;
+    }
+};
