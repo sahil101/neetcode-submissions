4f0f8d58520e23b4d13533ab106501ae601b463d
Sahil Agarwal <agarwalsahil97@gmail.com>
2026-08-25 22:46:37 +0530

    Add: binary-tree-right-side-view - submission-2

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-2.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        if (!root) return {};
+        vector<int> ans;
+        queue<pair<TreeNode*, int>> q;
+        q.push({root, 0});
+        while (!q.empty()) {
+            auto [node, level] = q.front();
+            q.pop();
+            if (level == ans.size()) ans.push_back(node->val);
+            else ans[level] = node->val;  
+            if (node->left)  q.push({node->left,  level + 1});
+            if (node->right) q.push({node->right, level + 1});
+        }
+        return ans;
+    }
+};
